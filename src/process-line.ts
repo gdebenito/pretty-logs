0e1c8a01bb5688fd9629dbe3ccc74556d87e9d9f
gon <gdebenitocassado@gmail.com>
2019-11-16 22:29:44 +0100

    Time added

--- src/process-line.ts
+++ src/process-line.ts
@@ -1,7 +1,9 @@
 import * as EventEmitter from 'events';
-import config from './../logs.json';
+import * as dateFormat from 'dateformat';
+import * as chalk from 'chalk';
+import * as config from '../simple.json';
 
-console.log(config)
+console.log(config);
 
 const processLine = new EventEmitter();
 
@@ -15,54 +17,89 @@ const processLine = new EventEmitter();
 //   // console.log(config);
 // };
 
-const regexFn = (value: any, regexConfig: any) => {
-  const regexKeys = Object.keys(regexConfig);
-  const firstMatch = regexKeys.filter((regexKey) => {
-    const regex = new RegExp(regexKey);
-    return regex.test(value);
-  }).map((regexKey: any) => {
-    console.log(regexKey);
-    return regexKey;
-  }).filter((_: any, idx: number) => idx === 0) as unknown;
-  const badCode = firstMatch as string;
-  return regexConfig[badCode];
-};
+// const regexFn = (value: any, regexConfig: any) => {
+//   const regexKeys = Object.keys(regexConfig);
+//   const firstMatch = regexKeys.filter((regexKey) => {
+//     const regex = new RegExp(regexKey);
+//     return regex.test(value);
+//   }).map((regexKey: any) => {
+//     console.log(regexKey);
+//     return regexKey;
+//   }).filter((_: any, idx: number) => idx === 0) as unknown;
+//   const badCode = firstMatch as string;
+//   return regexConfig[badCode];
+// };
 
 // TODO:type config
-const transform = (config: any) => {
-  const { keys } = config;
+// const transform = (config: any) => {
+//   const { keys } = config;
+//
+//   console.log(keys)
+//
+//   const valuesToTransform = Object.keys(keys);
+//
+//   return (logObject: any) => {
+//     const logKeys = Object.keys(logObject);
+//     const keysToPrint = logKeys.filter((logKey) => valuesToTransform.includes(logKey));
+//
+//     return keysToPrint.map((key: string) => {
+//       const value = logObject[key];
+//       const logConfig = config[key];
+//       if (logConfig.regex) {
+//         const printConfig = regexFn(value, logConfig.regex);
+//         console.log(printConfig);
+//       }
+//       return key;
+//     });
+//   };
+// };
 
-  console.log(keys)
+const formatEpochToFormat = (epoch: number, format: string):string => dateFormat(epoch, format);
 
-  const valuesToTransform = Object.keys(keys);
-
-  return (logObject: any) => {
-    const logKeys = Object.keys(logObject);
-    const keysToPrint = logKeys.filter((logKey) => valuesToTransform.includes(logKey));
-
-    return keysToPrint.map((key: string) => {
-      const value = logObject[key];
-      const logConfig = config[key];
-      if (logConfig.regex) {
-        const printConfig = regexFn(value, logConfig.regex);
-        console.log(printConfig);
-      }
-      return key;
-    });
-  };
+const colorMessage = (type:string, value: string, message:string) => {
+  let msg;
+  switch (type) {
+    case 'keyword':
+      msg = chalk.keyword(value)(message);
+      break;
+    // case 'hex':
+    //   msg = chalk.hex(value)(message);
+    // break;
+    // case 'rgb':
+    //   msg = chalk.rgb(...value)(message);
+    // break;
+    default:
+      msg = message;
+      break;
+  }
+  return msg;
 };
 
-let transformFn = transform( config['default'] )
-
-// (async () => {
-//   const conf = await getConfig();
-//   transformFn = transform(conf);
-// })();
+const transformJsonToBeautifulString = (input: any, conf: any) => {
+  const keys = Object.keys(conf);
+  const formatted = keys.filter((k) => !!input[k]).map((k) => {
+    const cfg = conf[k];
+    let log = '';
+    switch (cfg.input.format) {
+      case 'epoch':
+        log = formatEpochToFormat(input[k], cfg.output.format);
+        break;
+      default:
+        log = '';
+        break;
+    }
+    const { type, value } = cfg.output.color;
+    log = colorMessage(type, value, log);
+    return log;
+  }).join('');
+  return formatted;
+};
 
 processLine.on('line', (line: string) => {
   try {
     const json = JSON.parse(line);
-    transformFn(json);
+    const pretty = transformJsonToBeautifulString(json, config);
+    process.stdout.write(`${pretty}\n`);
     // console.log(json);
   } catch (err) {
     // unprocessable json
